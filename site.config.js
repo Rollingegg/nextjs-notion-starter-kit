803511e316c411d5ab495ff6a9b34b5abef7478d
Rollingegg <1244797125@qq.com>
2021-09-28 17:26:30 +0800

    customize site info

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '97114708ed38479080863c150dadc369',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '小小天地',
+  domain: 'rollingegg.it',
+  author: 'Rollingegg',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: '小小天地，大有作为',
+  socialImageTitle: 'Rollingegg',
+  socialImageSubtitle: 'Hey, here! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'rollingegg',
+  github: 'Rollingegg',
+  linkedin: 'null',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
